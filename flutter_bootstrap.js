9590ae6ed81b5cc0d934d7bd2b92efce2c8a1003
iAmMccc <562863544@qq.com>
2025-08-08 17:47:48 +0800

    更新内容

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"f73bfc4522dd0bc87bbcdb4bb3088082755c5e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2739951205"
+    serviceWorkerVersion: "4255755360"
   }
 });
